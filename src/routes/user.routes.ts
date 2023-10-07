5d0a224934f018adc0af95b3ca436d8af7754da3
Gabriel Azevedo <gabrielazevedo318@gmail.com>
2023-10-07 15:10:32 -0300

    Ajustes na rota de especialidade e configurado o swagger

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,15 +1,10 @@
-import { Router, response } from "express";
+import { Router } from "express";
 import { createUserController } from "../modules/users/useCases/create-user";
-import { createSpecialityController } from "../modules/useCases/create-speciality";
 
 const userRouter = Router();
 
 userRouter.post("/users", async (request, response) => {
   await createUserController.handle(request, response);
-});
-
-userRouter.post("/speciality", async (request, response) => {
-  await createSpecialityController.handle(request, response);
 });
 
 export { userRouter };
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import express from "express";
+import swaggerUI from "swagger-ui-express";
+
+import swaggerDocument from "../swagger.json";
 
 import { userRouter } from "./routes/user.routes";
+import { specialityRouter } from "./routes/speciality.routes";
 
 const app = express();
 
 app.use(express.json());
 
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 app.use(userRouter);
+app.use(specialityRouter);
 
 app.listen(3000, () => console.log("Server is running on PORT 3000"));
